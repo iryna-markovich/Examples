2c6b8ada1472a86068b68df0384f4e7b4b3955b8
Iryna Markovich <irina.markovich13@gmail.com>
2018-08-30 14:13:07 +0300

    Update

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,15 +1,14 @@
 function bubbleSort(arr){
-    for (let i = 0; i < arr.length - 1; i++){
-        for (let j = 0; j < arr.length - i - 1; j++){
+    for (let i = arr.length - 1; i > 0; i--){
+        for (let j = 0; j < i; j++){
             if (arr[j] > arr[j + 1]){
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
-            } else {}
+            }
         }
     }
     return arr;
-    console.log(arr);
 }
 
 bubbleSort([3,7,8,1,0,2,5]);
